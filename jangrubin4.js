ffdcbf28b4a9e887fedaf99cafd4e8539e621048
장루빈집 <wkdfnqls2465@gmail.com>
2023-04-20 22:52:19 +0900

    순서 변경

--- jangrubin4.js
+++ jangrubin4.js
@@ -25,15 +25,8 @@ function isExampleTwo(first, second) {
   // console.log(exampleOne(["대머리","독수리"], ["알파고","비비고"], 'string'))
   let arrayTypeCheck = exampleOne(2,3,"number")
  if(!arrayTypeCheck) {
- console.log("정상 실행")
- } else {
-  throw new Error("타입을 확인해주세요")
- }
-
-// ?isExampleTwo에 exampleOne함수를 호출해서 타입 검사해줌
-// ? 에러가 생기면 throw new Error
-
-  let booleanResult;
+//  실행
+let booleanResult;
 
   if(first < second) {
     booleanResult = true;
@@ -46,30 +39,50 @@ function isExampleTwo(first, second) {
   } else {
     return new Error("조건식이 boolean이 아니어서 리턴하지 못했습니다");
   }
-  return booleanResult
+
+
+ }
+  //타입검사 실패 
+ else {
+  throw new Error("타입을 확인해주세요")
+ }
+
+// ?isExampleTwo에 exampleOne함수를 호출해서 타입 검사해줌
+// ? 에러가 생기면 throw new Error
 }
 isExampleTwo(2,3)
 
 let basicData = [4,6,23,5,94,35,44,66];
 
 function exampleThree(array, functionOne, functionTwo) {
+    // array의 모든 원수는 숫자(정수)여야 함
   for(let i=0; i<array.length; i++) {
     if(typeof(array[i]) !== "number" || (array[i] | 0) !== array[i]) {
       throw new Error("array는 정수타입이어야 합니다")
     }
   }
+  // 매개 변수 인자로 들어오는 함수 두개는 데이터 타입이 모두 함수여야 함
   if(typeof(functionOne) !== "function" || typeof(functionTwo) !== "function") {
     throw new Error("functionOne, functionTwo는 함수 타입이어야 합니다")
-  }
-  // 매개 변수 인자로 들어오는 함수 두개는 데이터 타입이 모두 함수여야 함
-  // array의 모든 원수는 숫자(정수)여야 함
+  }  
+  // 실행 됐을 때
+  else {
+    let arrayResult = [[null, null],[null, null],[null, null],[null, null]];
   // array의 length는 짝수여야함, 홀수라면 10을 추가해서 짝수로 만듬
+  let arrayLengthCheck = arrayLengthEven(["ㄴㄴ"])
+  
+  }
+  
 
-  let arrayResult = [[null, null],[null, null],[null, null],[null, null]];
+  
 
   // 매개변수 array는 배열의 순서 한쌍씩 값의 크기를 비교하여
   // 비교한 작은 값을 arrayResult의 원소배열의 0번째
   // 큰 값은 arrayResult 원소배열의 첫번째에 넣어준다
+  
+  
+  
+  
 
   return arrayResult;
 }
