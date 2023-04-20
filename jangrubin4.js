bfc71266264130c2d8c4ba1bac9f043a479b0ca0
장루빈집 <wkdfnqls2465@gmail.com>
2023-04-20 22:11:30 +0900

    exampleThree 조건 생성

--- jangrubin4.js
+++ jangrubin4.js
@@ -3,9 +3,8 @@ function exampleOne(first, second, dataType) {
     return new Error(`매개변수 first, second 모두 ${dataType} 타입이어야 합니다.`);
   }
 }
-
-// ?exampleOne은 first, second배열 두개를 넣고 데이터 타입을 검사함
-// ?(밑의 내용으로 추측 가능)
+// console.log(exampleOne("ss","Ss","string"))
+// ?exampleOne은 first, second매개변수를 넣고 데이터 타입을 검사함
 // ?-> 입력한 데이터타입과 동일 해야함
 
 function arrayLengthEven(array) {
@@ -24,7 +23,7 @@ function isExampleTwo(first, second) {
   // 타입 검사가 실패하면 에러를 발생시켜 함수가 실행되지 않도록 조치함
   
   // console.log(exampleOne(["대머리","독수리"], ["알파고","비비고"], 'string'))
-  let arrayTypeCheck = exampleOne("ss","Ss","string")
+  let arrayTypeCheck = exampleOne(2,3,"number")
  if(!arrayTypeCheck) {
  console.log("정상 실행")
  } else {
@@ -47,12 +46,21 @@ function isExampleTwo(first, second) {
   } else {
     return new Error("조건식이 boolean이 아니어서 리턴하지 못했습니다");
   }
+  return booleanResult
 }
-isExampleTwo()
+isExampleTwo(2,3)
 
 let basicData = [4,6,23,5,94,35,44,66];
 
 function exampleThree(array, functionOne, functionTwo) {
+  for(let i=0; i<array.length; i++) {
+    if(typeof(array[i]) !== "number" || (array[i] | 0) !== array[i]) {
+      throw new Error("array는 정수타입이어야 합니다")
+    }
+  }
+  if(typeof(functionOne) !== "function" || typeof(functionTwo) !== "function") {
+    throw new Error("functionOne, functionTwo는 함수 타입이어야 합니다")
+  }
   // 매개 변수 인자로 들어오는 함수 두개는 데이터 타입이 모두 함수여야 함
   // array의 모든 원수는 숫자(정수)여야 함
   // array의 length는 짝수여야함, 홀수라면 10을 추가해서 짝수로 만듬
